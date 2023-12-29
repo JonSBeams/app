16e10e8e432849a8453fd27a17131b8878f4f99b
jgs01 <jonathan@beamsrenovation.com>
2023-12-29 11:26:55 +0000

    deal with update issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [textPrompt, setTextPrompt] = useState("");
+  const [prompts, setPrompts] = useState([]);
+
+  const generateImage = async () => {
+    const apiKey =
+      "cn6aNS64wQveEE1yZF0CTLhUOqm7OJtHlWQqqn7oigf0XtMFW483APiehP0h";
+    const url = "https://stablediffusionapi.com/api/v3/text2img";
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ key: apiKey, prompt: textPrompt }),
+    };
+
+    try {
+      const response = await fetch(proxyUrl + url, requestOptions);
+      const data = await response.json();
+
+      // Get the image URL from the response
+      const imageUrl = data.output[0];
+
+      // Update the prompts array with the generated image URL
+      setPrompts([...prompts, { prompt: textPrompt, imageUrl }]);
+    } catch (error) {
+      console.error("Error generating image:", error);
+    }
+  };
+
+  const handleClick = async () => {
+    setPrompts([...prompts, { prompt: textPrompt, imageUrl: "" }]);
+    setTextPrompt("");
+    await generateImage();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        className="input-container"
+        type="text"
+        placeholder="Enter a text prompt"
+        value={textPrompt}
+        onChange={(e) => setTextPrompt(e.target.value)}
+      />
+      <button onClick={handleClick}>Generate Image</button>
+      <ul className="prompts-list">
+        {prompts.map((item, index) => (
+          <li key={index} className="prompt-item">
+            <p className="prompt-text">{item.prompt}</p>
+            {item.imageUrl && (
+              <img
+                src={item.imageUrl}
+                alt="Generated Image"
+                className="generated-image"
+              />
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
